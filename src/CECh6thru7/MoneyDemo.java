ff4a5c93fb83fb176954d1fcb4a3081fec76084b
CodeZaru <teamsullivank@gmail.com>
2018-07-07 15:57:30 -0400

    code updated via Eclipse

--- src/CECh6thru7/MoneyDemo.java
+++ src/CECh6thru7/MoneyDemo.java
@@ -1,8 +1,30 @@
 
-public class MoneyDemo {
+public class MoneyDemo 
+{
+	public static void main(String[] args) 
+	{
+		Money start = new Money();
+		Money goal = new Money();
+		
+		System.out.println("Enter your current savings: ");
+		start.readInput();
+		
+		goal = start.times(2);
+		System.out.print(
+				"If you double that, you will have ");
+		goal.writeOutput();
+		
+		System.out.println(", or better yet:");
+		goal = start.add(goal);
+		System.out.println(
+				"If you triple that original amount, you will have:");
+		goal.writeOutput();
+		System.out.println();
+		
+		System.out.println("Remember: A penny saved");
+		System.out.println("is a penny earned.");
+		
 
-	public static void main(String[] args) {
-//see page 443 Listing 6.17
 	}
 
 }
