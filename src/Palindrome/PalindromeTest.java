efc1b567ace4930f7a588bf4bfee2589c1b4e9ae
CodeZaru <teamsullivank@gmail.com>
2018-07-09 14:41:21 -0400

    updated via Eclipse

--- src/Palindrome/PalindromeTest.java
+++ src/Palindrome/PalindromeTest.java
@@ -1,22 +1,25 @@
 
 public class PalindromeTest {
 	
-	public static int MAX_SIZE = 7;
+	public static int MAX_SIZE = 8;
 	
 	private int countOfEntries; //can be less than MAX_SIZE
 	
 //	declare array
-	private String[] pTArray;
+//	private String[] pTArray;
+	private char[] pTArray;
 	
 	PalindromeTest ()
 	{
-		pTArray = new String[MAX_SIZE];
+//		pTArray = new String[MAX_SIZE];
+		pTArray = new char[MAX_SIZE];
 		countOfEntries = 0;//keeps track of index for add entry method
 	}
 	
-	public void addItem(String input)
+//	public void addItem(String arrayEntry)
+	public void addItem(char arrayEntry)
 	{
-		pTArray[countOfEntries] = input;
+		pTArray[countOfEntries] = arrayEntry;
 		countOfEntries++;
 	}
 	
@@ -33,22 +36,30 @@ public class PalindromeTest {
 			int indexPositionTest1 = test1.countOfEntries;
 
 			//add Item to array
-			test1.addItem(input);
+			//test1.addItem(input);
+
 			
 			System.out.println("###");
 			for (int i=0; i < input.length(); i++)
 			{
 				inputChar = input.charAt(i);//input.length() -1);
+				test1.addItem(inputChar);
 				System.out.println(inputChar);
 			}	
 			System.out.println("###");
-			
-			//test that counter icrements after adding item
+
+			//test that counter increments after adding item
 			int indexPositionTest2 = test1.countOfEntries;
 			
 			//print array contents at input index
 			System.out.println(indexPositionTest1);
 			System.out.println(indexPositionTest2);
+
+			System.out.println("char at last position is:");
+//			System.out.println(test1.pTArray[6]);
+			System.out.println(test1.pTArray[indexPositionTest2-1]);
+
+	
 	}
 
 }
