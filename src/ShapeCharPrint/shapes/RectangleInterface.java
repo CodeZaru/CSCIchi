a3af24f83598031593fa71b05b3590163988f6a0
CodeZaru <teamsullivank@gmail.com>
2018-07-23 01:16:59 -0400

    updated code via Eclipse

---
+++ src/ShapeCharPrint/shapes/RectangleInterface.java
@@ -0,0 +1,7 @@
+package shapes;
+/** Interface for a rectangle to be drawn on the screen. */ 
+public interface RectangleInterface extends ShapeInterface 
+{ 
+	/** Sets the rectangle's dimensions. */ 
+	public void set(int newHeight, int newWidth); 
+	} 
---
+++ src/ShapeCharPrint/shapes/ShapeBase.java
@@ -0,0 +1,18 @@
+package shapes;
+//import interfaces.ShapeInterface;
+
+/**
+ * Abstract base class for drawing simple shapes on the screen using characters.
+ */
+
+public abstract class ShapeBase implements ShapeInterface {
+	private int offset;
+
+	public abstract void drawHere();
+
+	public void drawAt(int lineNumber) {
+		for (int count = 0; count < lineNumber; count++)
+			System.out.println();
+		drawHere();
+	}
+}
